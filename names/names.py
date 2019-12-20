e764d58357c9772fb9b1f37ada6505229bd40042
johnschneider1 <johnlawschneider@gmail.com>
2019-12-20 12:59:23 -0600

    should be there

--- names/names.py
+++ names/names.py
@@ -1,4 +1,7 @@
+from binary_search_tree import BinarySearchTree
 import time
+import sys
+# sys.path.append('../binary_search_tree')
 
 start_time = time.time()
 
@@ -11,14 +14,23 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+tree = BinarySearchTree(names_1[0])
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+# original solution is slow at 6 seconds 0(n^2), my solution should be, 0(log(n))average or worst 0(n) as it's a linear
+for name in names_1:
+    tree.insert(name)
+
+for name in names_2:
+    if tree.contains(name):
+        duplicates.append(name)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,5 +1,8 @@
 from doubly_linked_list import DoublyLinkedList
 
+# ring buffer is fixed size, when it fills up adding another element overwrites
+# newest head, tail o
+
 
 class RingBuffer:
     def __init__(self, capacity):
@@ -8,11 +11,28 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # check size to capacity
+        if self.storage.length < self.capacity:
+            # replace value of previous
+            self.storage.add_to_head(item)
+            # update current node is
+            self.current = self.storage.tail
+        else:
+            self.current.value = item
+            self.current = self.current.prev
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
+        current_node = self.storage.tail
+
+        while current_node:
+            if current_node.value is not None:
+                # add the current item
+                list_buffer_contents.append(current_node.value)
+            current_node = current_node.prev
+
+        print(list_buffer_contents)
 
         # TODO: Your code here
 
